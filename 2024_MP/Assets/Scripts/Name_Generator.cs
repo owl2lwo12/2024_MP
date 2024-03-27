fb3a496ef06d860cc8f7e90cfe9f06e158a6b6dd
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2024-03-27 22:48:45 +0900

    PUT: Name_Generator, Pitcher_Stats

---
+++ 2024_MP/Assets/Scripts/Name_Generator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "name_generator", menuName = "ScriptableObject/name_generator", order = 2)]
+public class Name_Generator : ScriptableObject
+{
+    [SerializeField]
+    private string[] familyname; //성 저장할 것
+    [SerializeField]
+    private string[] lastname; //이름 저장할 것
+
+    public string GenerateName()
+    {
+        string fname = familyname[Random.Range(0, familyname.Length)]; //familyname 중 하나를 랜덤하게 가져올 것
+        string lname = lastname[Random.Range(0, lastname.Length)]; //lastname 중 하나를 또한 가져올 것
+        string name = fname + lname; //두개를 합칠 것
+        return name;
+    }
+}
--- 2024_MP/Assets/Scripts/Pitcher_Stats.cs
+++ 2024_MP/Assets/Scripts/Pitcher_Stats.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "pitcher_stats", menuName = "ScriptableObject/pitcher_stats", order = 1)]
 public class Pitcher_Stats : ScriptableObject
 {
     //stats
@@ -20,7 +21,12 @@ public class Pitcher_Stats : ScriptableObject
     [SerializeField]
     private bool hand = false; // false = right , true = left
     [SerializeField]
-    private int age = 20;
+    private int age = 20; //현재 나이
+    [SerializeField]
+    private int g_age = 30; // 성장할 수 있는 나이(?) 이 나이 이후로는 성장이 거의 없다시피 할 것.
+    [SerializeField]
+    private int c_age = 37; // 에이징 커브가 시작 될 나이. 이때부터 능력치가 감소하기 시작한다.
+
 
     // Constructor
     public Pitcher_Stats()
@@ -33,7 +39,36 @@ public class Pitcher_Stats : ScriptableObject
         control = Random.Range(0,pControl + 1);
         stamina = Random.Range (0,pStamina + 1);
 
+        g_age = Random.Range(-2, 3) + 30;
+        c_age = Random.Range(-2, 3) + 37;
+
         if (Random.Range(0, 100) > 75) hand = true;
         else hand = false;
+
+        if (Random.Range(0, 100) > 90) age = 23;
+        else age = 20;
     }
+    public void ReuseData()
+    {
+        pSpeed = Random.Range(20, 101);
+        pStamina = Random.Range(20, 101);
+        pControl = Random.Range(20, 101);
+
+        speed = Random.Range(0, pSpeed + 1);
+        control = Random.Range(0, pControl + 1);
+        stamina = Random.Range(0, pStamina + 1);
+
+        g_age = Random.Range(-2, 3) + 30;
+        c_age = Random.Range(-2, 3) + 37;
+
+        if (Random.Range(0, 100) > 75) hand = true;
+        else hand = false;
+
+        if (Random.Range(0, 100) > 90) age = 23;
+        else age = 20;
+    } 
+    //both Pitcher_Stats and ReuseData are use for make new pitchers but one is constructor and 
+    //other is for reuse Pitcher_Stats() class objects if pitcher's career is end. instead delete data.
+    
+
 }
