2e4a94dd940a33a4a668a2e21182d2143d32a910
LeeJangHyun <leejanghyun11@gmail.com>
2024-05-27 05:41:44 +0900

    [feat] Team Game Result

--- 2024_MP/Assets/Scripts/UI/02. Team/TeamPanelManager.cs
+++ 2024_MP/Assets/Scripts/UI/02. Team/TeamPanelManager.cs
@@ -8,15 +8,17 @@ public class TeamPanelManager : Singleton<TeamPanelManager>
     public TMP_Text gameTxt;
     public TMP_Text winTxt;
     public TMP_Text loseTxt;
+    public TMP_Text drawTxt;
     public TMP_Text rateTxt;
     public TMP_Text rankTxt;
 
-    public void SettingTeamInformationText()
+    public void SettingTeamInformationText(int game, int win, int lose, int draw, float rate, int rank)
     {
-        /*gameTxt.text = 
-        winTxt.text = 
-        loseTxt.text = 
-        rateTxt.text = 
-        rankTxt.text = */
+        gameTxt.text = $"{game}";
+        winTxt.text = $"{win}";
+        loseTxt.text = $"{lose}";
+        drawTxt.text = $"{draw}";
+        rateTxt.text = $"{rate}";
+        rankTxt.text = $"{rank}";
     }
 }
