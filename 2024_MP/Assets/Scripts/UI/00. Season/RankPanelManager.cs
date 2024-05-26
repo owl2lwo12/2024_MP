fff22c8f872d11220b82ba90ec99b15b0311be67
LeeJangHyun <leejanghyun11@gmail.com>
2024-05-27 05:35:37 +0900

    [feat] Ranking Feature

--- 2024_MP/Assets/Scripts/UI/00. Season/RankPanelManager.cs
+++ 2024_MP/Assets/Scripts/UI/00. Season/RankPanelManager.cs
@@ -1,10 +1,31 @@
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI._00._Season
 {
     public class RankPanelManager : MonoBehaviour
     {
-        public List<RankManager> rankManagers = new List<RankManager>();
+        public List<Image> teamIcons = new List<Image>(10);
+        public List<TMP_Text> teamNames = new List<TMP_Text>(10);
+
+        public int[] ranking = new int[10];
+        
+        private void Update()
+        {
+            ranking = MainSystem._instance.Ranking;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int team = ranking[i];
+
+                teamIcons[i].sprite = UIManager.Instance.TeamSprites[team];
+                teamIcons[i].SetNativeSize();
+                teamNames[i].text = UIManager.Instance.TeamNames[team];
+            }
+            
+        }
     }
 }
