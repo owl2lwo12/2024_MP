2b5ddc470424a4032f9aca43a6b8df69abba4386
LeeJangHyun <leejanghyun11@gmail.com>
2024-05-27 05:50:12 +0900

    [feat] 전적 표시

--- 2024_MP/Assets/Scripts/UI/02. Team/TeamPanelManager.cs
+++ 2024_MP/Assets/Scripts/UI/02. Team/TeamPanelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,6 +12,20 @@ public class TeamPanelManager : Singleton<TeamPanelManager>
     public TMP_Text drawTxt;
     public TMP_Text rateTxt;
     public TMP_Text rankTxt;
+
+    private Team_Scripts _myTeam;
+    
+    private void Update()
+    {
+        _myTeam = MainSystem._instance.teams[0].GetComponent<Team_Scripts>();
+        int game = MainSystem._instance.Match_num;
+        int win = _myTeam.Win;
+        int lose = _myTeam.Lose;
+        int draw = _myTeam.Draw;
+        float rate = _myTeam.WinRate;
+        int rank = _myTeam.Rank;
+        SettingTeamInformationText(game, win, lose, draw, rate, rank);
+    }
 
     public void SettingTeamInformationText(int game, int win, int lose, int draw, float rate, int rank)
     {
