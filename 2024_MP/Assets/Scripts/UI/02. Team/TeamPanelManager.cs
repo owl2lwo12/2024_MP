65ab923b052c08b8e7a5d7742520e7c34a26578e
LeeJangHyun <leejanghyun11@gmail.com>
2024-05-27 00:26:01 +0900

    [feat] 팀 패널

---
+++ 2024_MP/Assets/Scripts/UI/02. Team/TeamPanelManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TeamPanelManager : Singleton<TeamPanelManager>
+{
+    public TMP_Text gameTxt;
+    public TMP_Text winTxt;
+    public TMP_Text loseTxt;
+    public TMP_Text rateTxt;
+    public TMP_Text rankTxt;
+
+    public void SettingTeamInformationText()
+    {
+        /*gameTxt.text = 
+        winTxt.text = 
+        loseTxt.text = 
+        rateTxt.text = 
+        rankTxt.text = */
+    }
+}
